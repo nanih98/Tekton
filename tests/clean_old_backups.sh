0df1d939e5e036aab7775d2844a0f9f00d044130
Daniel Cascales Romero <daniel.cascales@soprasteria.com>
2021-05-21 10:50:49 +0200

    Wip

--- tests/clean_old_backups.sh
+++ tests/clean_old_backups.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-dir="/copiaseguridad/backups"
-find $dir -type f -mtime +1 | xargs -I {} ls -ltrah {}
+dir="/copiaseguridad/backups/*"
+find $dir -type d -mmin +1 | xargs -I {} ls -ltrah {}
+
+#find $dir -type d -mmin +1 | xargs -I {} ls -ltrah {}
