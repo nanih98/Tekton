af2143284950f5ba13c4580f343e4e2bc8b137d4
Daniel Cascales Romero <daniel.cascales@soprasteria.com>
2021-05-25 16:27:12 +0200

    Wip

--- tests/restura_wordpress.sh
+++ tests/restura_wordpress.sh
@@ -2,10 +2,10 @@
 
 # Restaurar backup de wordpress y mariadb
 echo ${1}
-#dir="/copiaseguridad/backups/${1}"
-#
+dir="/copiaseguridad/backups/copiaseguridad_${1}"
 #rm -rf /var/www/html
-#cd $dir
-#echo $dir
-#echo -e "Recuperando backup de wordpress con fecha ${1}"
-#tar -xf contenidowp.tar.gz -C /tmp/wordpress # /var/www/html
+
+cd $dir
+echo $dir
+echo -e "Recuperando backup de wordpress con fecha ${1}"
+tar -xf contenidowp.tar.gz -C /tmp/wordpress # /var/www/html
